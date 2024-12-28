5f27eea15f2deb4d18ebe9216ff4877b586bc086
Ashwin454 <ashwinj@iitbhilai.ac.in>
2024-12-28 20:51:40 +0530

    FC

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app=require("./app");
 const dotenv=require("dotenv");
+const express = require('express')
 const { connectDB } = require("./config/database");
 
 dotenv.config({path: "./config/config.env"});
@@ -8,4 +9,14 @@ connectDB();
 
 app.listen(8080 , ()=>{
     console.log(`Server is UP.`);
+});
+
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The following must be placed after all your API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
